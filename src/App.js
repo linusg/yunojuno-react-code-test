e01cb7ec121dd1239660ff161d1e7cc3f41369cf
Al Scott <al@yunojuno.com>
2018-03-26 16:36:25 +0100

    add YJ trim

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import characters from "./fixtures/characters.json";
+
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
+                    <h1 className="App-title">
+                        Lord of the Rings Character Index
+                    </h1>
                 </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to
-                    reload.
-                </p>
+
+                <section>{/* Lovely character list goes here */}</section>
             </div>
         );
     }
